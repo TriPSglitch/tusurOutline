671f63cdb237774faa262aa0219d9bc90c51e969
Максим Е. Карбышев <mek@2i.tusur.local>
2026-01-16 14:20:51 +0700

    dumb try without override docker:

--- run-with-tusur.sh
+++ run-with-tusur.sh
@@ -10,15 +10,15 @@ sudo nginx -t
 sudo systemctl reload nginx
 
 # Останавливаем и удаляем старые контейнеры
-sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml down                                      # 
+sudo docker-compose -f docker-compose.yml down                                      # -f docker-compose.override.yml
 sudo docker rmi outline_outline outlinewiki/outline:latest 2>/dev/null || true
 sudo docker volume prune -f
 
 # Собираем кастомный образ Outline
-sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml build                                     #
+sudo docker-compose -f docker-compose.yml build                                     # -f docker-compose.override.yml
 
 # Запускаем
-sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d                                     #
+sudo docker-compose -f docker-compose.yml up -d                                     # -f docker-compose.override.yml
 
 # Проверяем логи
 sudo docker-compose logs -f outline
